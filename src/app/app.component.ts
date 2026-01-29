dd1e5b2ebef5fd34a7d4dec23be49c1bd2d6cf93
Julian Herzfeld <herzfeldjulian@hotmail.de>
2026-01-29 04:25:20 +0100

    first commit, basic structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/component/navbar/navbar.component';
+import { FooterComponent } from './shared/component/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
