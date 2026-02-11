4a3cf8189ae0dfc9df96e30c449195002aaedc1a
Julian Herzfeld <herzfeldjulian@hotmail.de>
2026-02-11 03:12:08 +0100

    responsive design started, translate legal notice and privacy policy,

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-legal-notice',
@@ -7,6 +10,33 @@ import { Component } from '@angular/core';
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
-export class LegalNoticeComponent {
+export class LegalNoticeComponent implements OnInit, OnDestroy {
+
+  content = '';
+  private langSub?: Subscription;
+
+
+  constructor(
+    private http: HttpClient,
+    private translate: TranslateService
+  ) { }
+
+  ngOnInit() {
+    this.loadPrivacy(this.translate.currentLang || 'de');
+
+    this.langSub = this.translate.onLangChange.subscribe(e => {
+      this.loadPrivacy(e.lang);
+    });
+  }
+
+  ngOnDestroy() {
+    this.langSub?.unsubscribe();
+  }
+
+  private loadPrivacy(lang: string) {
+    this.http
+      .get(`/assets/legal/imprint.${lang}.html`, { responseType: 'text' })
+      .subscribe(html => this.content = html);
+  }
 
 }
--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-privacy-policy',
@@ -7,6 +10,31 @@ import { Component } from '@angular/core';
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
-export class PrivacyPolicyComponent {
+export class PrivacyPolicyComponent implements OnInit, OnDestroy {
 
+  content = "";
+  private langSub?: Subscription;
+
+  constructor(
+    private http: HttpClient,
+    private translate: TranslateService
+  ) { }
+
+  ngOnInit() {
+    this.loadPrivacy(this.translate.currentLang || 'en');
+
+    this.langSub = this.translate.onLangChange.subscribe(e => {
+      this.loadPrivacy(e.lang);
+    });
+  }
+
+  ngOnDestroy() {
+    this.langSub?.unsubscribe();
+  }
+
+  private loadPrivacy(lang: string) {
+    this.http
+      .get(`/assets/legal/privacy.${lang}.html`, { responseType: 'text' })
+      .subscribe(html => this.content = html);
+  }
 }
